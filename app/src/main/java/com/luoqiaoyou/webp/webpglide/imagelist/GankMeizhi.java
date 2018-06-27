c2c9f2b440dcdc0578cf340234c186b9d0eb2fe1
Winter <XunMengWinter@gmail.com>
2018-06-27 18:52:46 +0800

    add list，添加网图测试。

---
+++ app/src/main/java/com/luoqiaoyou/webp/webpglide/imagelist/GankMeizhi.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2015 Drakeet <drakeet.me@gmail.com>
+ *
+ * This file is part of Meizhi
+ *
+ * Meizhi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meizhi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Meizhi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.luoqiaoyou.webp.webpglide.imagelist;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created on 2018/4/18.
+ *
+ * @author ice
+ */
+public class GankMeizhi implements Serializable {
+
+    public boolean error;
+    public List<ResultsBean> results;
+
+    public static class ResultsBean {
+        public String _id;
+        public String createdAt;
+        public String desc;
+        public String publishedAt;
+        public String type;
+        public String url;
+        public boolean used;
+        public String who;
+        public String source;
+    }
+}
